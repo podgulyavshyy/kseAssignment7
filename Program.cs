1973b6d078209d0f9a67d82a3a38b5125acff3cb
podgulyavshyy <mhunko@yahoo.com>
2022-05-11 15:59:57 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿string text;
+Console.Write("Enter sentence: ");
+text = Console.ReadLine();
+
+var punctuation = text.Where(Char.IsPunctuation).Distinct().ToArray();
+var words = text.Split().Select(x => x.Trim(punctuation));
+
+List<string> wordsFromFile = new List<string>();
+foreach(string line in File.ReadLines(@"../../../words_list.txt"))
+{
+    wordsFromFile.Add(line);
+}
+
+Console.Write("Looks like you have typos in next words: ");
+foreach(var item in words)
+{
+    if(!wordsFromFile.Contains(item))
+    {
+        
+        Console.Write(" " + item);
+    }
+}
